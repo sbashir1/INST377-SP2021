06634608b63744a2e1c69266a2dd7c5b88206b6e
Saamia Bashir <sbashir1@terpmail.umd.edu>
2021-02-11 22:28:00 -0500

    Make images horizontal

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,16 @@
 /* Put your javascript in here */
+const arrow_right = document.querySelector('right');
+const arrow_left = document.querySelector('left');
+let array = document.querySelectorAll('li');
+
+arrow_right.addEventListener('click', (event) => {
+
+});
+
+arrow_right.addEventListener('click', (event) => {
+
+});
+
+array.forEach(element => {
+    element
+});
