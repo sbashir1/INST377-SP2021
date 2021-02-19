11e1597e214f08848177a5157b4cfd002241a5b3
Saamia Bashir <sbashir1@terpmail.umd.edu>
2021-02-19 13:48:17 -0500

    Redid hooking up keyboard keys

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -14,9 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   let timerId = setInterval(startGame, 20);
 
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump()
+    }
+  }
+
   function jump() {
     if (birdBottom < 500) birdBottom += 50;
     bird.style.bottom = birdBottom + 'px';
   }
-  document.addEventListener('keyup', jump);
+  document.addEventListener('keyup', control);
 });
