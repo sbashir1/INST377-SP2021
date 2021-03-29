7ca585671018cce15cc9aac5f16c50a61bf701ad
Saamia Bashir <sbashir1@terpmail.umd.edu>
2021-03-29 16:09:26 -0400

    Creating the platforms

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -4,12 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+
+    class Platform {
+        
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++) {
+            let platformGap = 600 / platformCount
+            let newPlatformBottom = 100 + i * platformGap
+            let newPlatform = new Platform()  
+        }
     }
 
     function start() {
