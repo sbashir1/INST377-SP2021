95dcbe4b5d0862a311029a6e3ebba2bd16879a6b
Saamia Bashir <sbashir1@terpmail.umd.edu>
2021-03-29 16:20:02 -0400

    Creating the platform class

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerBottomSpace = 150
     let isGameOver = false
     let platformCount = 5
+    let platforms = []
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -14,14 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     class Platform {
-        
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
     }
 
     function createPlatforms() {
         for (let i = 0; i < platformCount; i++) {
             let platformGap = 600 / platformCount
             let newPlatformBottom = 100 + i * platformGap
-            let newPlatform = new Platform()  
+            let newPlatform = new Platform(newPlatformBottom)
+            platforms.push(newPlatform)  
         }
     }
 
